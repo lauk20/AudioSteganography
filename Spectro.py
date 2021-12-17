cdb4fe0bd2cf2b12555bda2c45895a5c29fa1b86
Kenny Lau <klau20@stuy.edu>
2021-12-17 15:33:36 -0500

    Started new function, does not work.

--- Spectro.py
+++ Spectro.py
@@ -4,6 +4,45 @@ import array
 from scipy.io import wavfile
 import numpy as np
 
+def newMain():
+    sampleRate = 44100;
+    audioLength = 15; #seconds
+    totalSamples = sampleRate * audioLength;
+
+    image = Image.open("lambo.jpg").convert("L");
+    pixels = image.load();
+    width, height = image.size;
+
+    audio = wave.open("newSpectro.wav", "w");
+    audio.setnchannels(1);
+    audio.setsampwidth(2); #2 bytes, 16 bit-depth
+    audio.setframerate(sampleRate);
+
+    maxFreq = 17000;
+    minFreq = 7000;
+    freqRange = maxFreq - minFreq;
+    samplesPerColumn = math.floor(totalSamples / width);
+    samplesPerPixel = math.floor(samplesPerColumn / height);
+
+    result = array.array('h');
+    print(samplesPerColumn);
+    print(samplesPerPixel);
+
+    for x in range(width):
+        print(x);
+        for y in range(height):
+            temp = [];
+            intensity = image.getpixel((x,y));
+            #print(intensity, frequency, freqRange);
+            for i in range(samplesPerPixel):
+                #print(i);
+                sample = intensity * math.sin(((freqRange/height) * (height - y) + minFreq) * 2 * math.pi * i);
+                result.append(int(math.floor(sample)));
+                #print(int(math.floor(sample)));
+            #result.append(temp);
+    audio.writeframes(result);
+
+
 def main():
     sampleRate = 44100; #sampling rate
     audioLength = 60 * 2; #seconds
@@ -58,4 +97,4 @@ def main():
     #print(counter);
     print("DONE");
     audio.close();
-main();
+newMain();
