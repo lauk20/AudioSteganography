8460c50b300e9825846a21dad6cdb34de3460bb5
David Chong <djchong242gmail.com>
2021-12-16 22:43:45 -0500

    made basic spectrogram

--- Spectrogram/spectrogram.py
+++ Spectrogram/spectrogram.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
+Fs, aud = wavfile.read('xxx.wav')
+
+aud = aud[:,0]
+
+powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(aud, Fs=Fs)
+plt.show()
