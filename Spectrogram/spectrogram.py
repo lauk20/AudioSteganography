40b6c5bd3e207db9ac83c189b10548dfa1fd2e94
DavidASC20 <djchong24@gmail.com>
2021-12-20 15:10:36 -0500

    scrapping spectrogram idea, can just use audacity

--- Spectrogram/spectrogram.py
+++ Spectrogram/spectrogram.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
-Fs, aud = wavfile.read('xxx.wav')
 
+Fs, aud = wavfile.read('xxx.wav')
 aud = aud[:,0]
 
 powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(aud, Fs=Fs)
+plt.xlabel("bruh")
+plt.ylabel("bruh.2")
 plt.show()
