884c06c4bae0a9f12aaed7e7b128932cab673281
lauk20 <klau20@stuy.edu>
2021-12-21 21:59:49 -0500

    Fixed the program to write a chunk to the end of the file that contains a raw message

--- alternate.c
+++ alternate.c
@@ -13,8 +13,8 @@ void encode(char * filename){
     char var[5] = "LENO";
     write(fd, var, 4);
 
-    char message[1000] = "Greed will be the death of us all.";
-    int size = strlen(message + 1);
+    char message[1000] = "Greed will be the death of us all.a";
+    int size = strlen(message) + 1;
     write(fd, &size, 4);
     write(fd, message, size);
 }
@@ -23,9 +23,9 @@ void decode(char * filename){
   int fd = open(filename, O_RDONLY, 0644);
   char riff_header[36];
   read(fd, riff_header, 36);
-
   int found_data = 1; // 0 means "data" chunk has been found.
   int datachunk_size = 0;
+
   while (found_data) {
     char chunk_descriptor[5];
     read(fd, chunk_descriptor, 4);
@@ -42,19 +42,29 @@ void decode(char * filename){
 
       char * ignored_chunk_data = calloc(size, 1);
       read(fd, ignored_chunk_data, size); //ignores the unecessary chunk (skips it);
-
+      free(ignored_chunk_data);
     }
   }
+  //char data[datachunk_size]; //breaks with this for some reason. (segfault)
+  char * data = calloc(datachunk_size, 1);
+  read(fd, data, datachunk_size);
+
   char var[5];
   int chunksize = 0;
   read(fd, var, 4);
   read(fd, &chunksize, 4);
-  char message[chunksize];
+
+  char * message = calloc(chunksize, 1);
+  strcpy(message, "");
   read(fd, message, chunksize);
+
   printf("%s\n", message);
+
+  free(message);
+  free(data);
 }
 
 int main(){
-  encode("secretchunk.wav");
-  decode("secretchunk.wav");
+  encode("furelisecopy.wav");
+  decode("furelisecopy.wav");
 }
